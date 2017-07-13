c93e5dd3627c574362d1cd1cc5e331f6babe6da4
Tyler Parsons <tparsons17@bloomberg.net>
2017-07-13 10:29:49 -0400

    Clean up creation and info records upon user deletion

--- script/bulk_delete_users.sh
+++ script/bulk_delete_users.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "usage: bulk_delete_users.sh <usernames_file>"
+if [ $# -ne 3 ]; then
+    echo "usage: bulk_delete_users.sh <usernames_file> <db_host> <db_port>"
     exit 3
 fi
 
 USERNAMES_FILE=$1
+DB_HOST=$2
+DB_PORT=$3
+
 USERNAMES=($(cat $USERNAMES_FILE | tr "\n" " "))
 
 for USERNAME in ${USERNAMES[@]}
@@ -16,6 +19,8 @@ do
     if [ $RC -ne 0 ]; then
         echo "WARNING: bundle exec rake user:deletion:by_username['$USERNAME'] failed with RC=$RC"
     fi
+    psql -U postgres -d carto_db_development -h $DB_HOST -p $DB_PORT -c  "delete from user_creations where username='$USERNAME';"
+    psql -U postgres -d carto_db_development -h $DB_HOST -p $DB_PORT -c  "delete from user_infos where username='$USERNAME';"
 done
 
 
