7219193b04d9185320ea864866e3ae1da812800e
Tyler Parsons <tparsons17@bloomberg.net>
2017-11-15 11:07:53 -0500

    Add profiles_users primary key

--- db/migrate/20171114183247_profiles_users.rb
+++ db/migrate/20171114183247_profiles_users.rb
@@ -2,6 +2,7 @@ Sequel.migration do
 
   up do
     create_table(:profiles_users) do
+      primary_key [:user_id, :profile_id], :name => :profiles_users_user_id_profile_id_pkey
       foreign_key :profile_id, :profiles, :type=>"uuid", :null=>false, :key=>[:id]
       foreign_key :user_id, :users, :type=>"uuid", :null=>false, :key=>[:id]
     end
